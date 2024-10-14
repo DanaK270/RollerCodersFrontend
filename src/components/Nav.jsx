061b3c6ec4a0b50088f6c8b78fe4327efb9ba3b7
isaaleid <isa.al.eid@gmail.com>
2024-10-15 01:13:34 +0300

    Components & Pages

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "../App.css";
+
+const Nav = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    navigate(`/parks?search=${encodeURIComponent(searchQuery)}`);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Tt</div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/parks">Parks</Link>
+        <Link to="/logIn">Log In</Link>
+
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Explore Parks..."
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.querySelector("#root")).render(
+  <Router>
+    <App />
+  </Router>
+);
