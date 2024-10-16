35dcf5249272654479fb50078190f3c0719696b3
Danak270 <daanaa227700@gmail.com>
2024-10-16 10:56:42 +0300

    add fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import FileUploader from './FileUploader'
+
 const AddParkForm = () => {
   const [formData, setFormData] = useState({
-    park: '',
     name: '',
-    // location: '',
-    country: '',
-    type: '',
-    // timezone: '',
+    countryCode: '',
+    parkType: '',
     description: '',
     image: null
   })
+  // const [file, setFile] = useState()
+
+  // const sendImage = async (e, file, description) => {
+  //   e.preventDefault()
+
+  //   const formData = new FormData()
+  //   formData.append('image', file)
+
+  //   const result = await axios.post(
+  //     'http://localhost:8080/api/images',
+  //     formData,
+  //     {
+  //       headers: { 'Content-Type': 'multipart/form-data' }
+  //     }
+  //   )
+  //   console.log(result.data)
+  // }
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -20,26 +36,30 @@ const AddParkForm = () => {
     })
   }
 
-  const handleFileChange = (e) => {
+  const handleFileChange = (e, file) => {
     setFormData({
       ...formData,
-      images: e.target.files
+      image: file
     })
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const res = await axios.post('http://localhost:4000/themeparks/addpark', {
-      formData
-    })
-    console.log(formData)
+    const result = await axios.post(
+      'http://localhost:4000/themeparks/addpark',
+      formData,
+      {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      }
+    )
+    console.log(result.data)
   }
 
   return (
     <div>
       <h1>Add a New Park</h1>
 
-      <form onSubmit={handleSubmit} encType="multipart/form-data">
+      <form onSubmit={handleSubmit} style={{ marginBottom: '50px' }}>
         <div></div>
 
         <div>
@@ -52,22 +72,12 @@ const AddParkForm = () => {
           />
         </div>
 
-        {/* <div>
-          <label>Location</label>
-          <input
-            type="number"
-            name="location"
-            value={formData.location}
-            onChange={handleChange}
-          />
-        </div> */}
-
         <div>
           <label>Country Code</label>
           <input
             type="text"
-            name="country"
-            value={formData.country}
+            name="countryCode"
+            value={formData.countryCode}
             onChange={handleChange}
           />
         </div>
@@ -76,21 +86,11 @@ const AddParkForm = () => {
           <label>Park Type</label>
           <input
             type="text"
-            name="type"
-            value={formData.type}
+            name="parkType"
+            value={formData.parkType}
             onChange={handleChange}
           />
         </div>
-        {/* 
-        <div>
-          <label>Time Zone</label>
-          <input
-            type="text"
-            name="timezone"
-            value={formData.timezone}
-            onChange={handleChange}
-          />
-        </div> */}
 
         <div>
           <label>Description</label>
@@ -105,12 +105,12 @@ const AddParkForm = () => {
         <div>
           <label>Image</label>
           <input
+            filename={formData.countryCode.image}
             name="image"
+            onChange={(e) => handleFileChange(e, e.target.files[0])}
             type="file"
             accept="image/*"
-            multiple
-            onChange={handleFileChange}
-          />
+          ></input>
         </div>
 
         <button type="submit">Add Park</button>
