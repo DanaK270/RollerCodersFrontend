a80db282c9da3584a311d747a0e69728a6066cef
Meryam313 <em.mohmed.24@gmail.com>
2024-10-16 13:08:51 +0300

    css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import Rollerlogo from '../assets/Rollerlogo.png';
+import Rollerlogo from '../assets/12.png';
 import '../App.css';
 
 const Nav = () => {
